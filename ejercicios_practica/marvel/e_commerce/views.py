63a5afb19c2431dfab9f4943623707f5440d6e4b
augustomagallanes <167917671+augustomagallanes@users.noreply.github.com>
2024-09-05 21:39:18 -0300

    Add files via upload

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -9,7 +9,12 @@ def comic_list_api_view(request):
         # Deberá completar el funcionamiento de este endpoint.
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
+        queryset = Comic.objects.all()
+        data = list(queryset.values('description', 'id', 'marvel_id', 'picture', 'price', 'stock_qty', 'title', 'wishlist'))
+
         print("Endpoint: comic_list_api_view")
+
+        return JsonResponse(data, safe=False)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -20,7 +25,14 @@ def comic_filter_stock_api_view(request):
         # Deberá completar el funcionamiento de este endpoint.
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
+
+        queryset = Comic.objects.filter(stock_qty=5)
+
+        data = list(queryset.values('description', 'id', 'marvel_id', 'picture', 'price', 'stock_qty', 'title', 'wishlist'))
         print("Endpoint: comic_filter_stock_api_view")
+
+        return JsonResponse(data, safe=False)
+    
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -31,7 +43,13 @@ def comic_filter_price_api_view(request):
         # Deberá completar el funcionamiento de este endpoint.
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
+
+        queryset = Comic.objects.filter(price__gt=3)
+
+        data = list(queryset.values('description', 'id', 'marvel_id', 'picture', 'price', 'stock_qty', 'title', 'wishlist'))
         print("Endpoint: comic_filter_price_api_view")
+        return JsonResponse(data, safe=False)
+        
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -42,6 +60,13 @@ def comic_list_order_api_view(request):
         # Deberá completar el funcionamiento de este endpoint.
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
-        print("Endpoint: comic_list_order_api_view")
+
+        queryset = Comic.objects.order_by('marvel_id')
+        data = list(queryset.values('description', 'id', 'marvel_id', 'picture', 'price', 'stock_qty', 'title', 'wishlist'))
+        print("Endpoint: comic_filter_price_api_view")
+
+        return JsonResponse(data, safe=False)
+        
+
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
